4a2f1fa1d283ae7682314d55a484d13abefa7ffd
Ben Kehoe <ben@kehoe.io>
2023-02-21 16:13:36 +0000

    v2.2

---
+++ gltest/go.mod
@@ -0,0 +1,3 @@
+module github.com/benkehoe/golangtest/gltest/v2
+
+go 1.19
---
+++ gltest/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+var Version string = "2.2"
+
+func main() {
+    fmt.Println("Test", Version)
+}
